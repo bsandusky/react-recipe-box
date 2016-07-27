9a6946ca0838b90900983d200cefc99e060c2513
bsandusky <brett.sandusky@gmail.com>
2016-07-27 13:23:38 -0400

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>Recipe Box!</div>
+    );
+  }
+}
