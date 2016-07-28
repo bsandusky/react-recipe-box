b87d61f7b44a08c31708c08704616df87fe9ace1
bsandusky <brett.sandusky@gmail.com>
2016-07-28 09:49:15 -0400

    clean up search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,10 @@ import React from 'react';
 const SearchBar = (props) => {
 
   return (
-    <div className="search-bar">
-      <input />
+    <div className="search-bar col-md-4">
+      <section id="search">
+        <input id="search-input"/>
+      </section>
     </div>
   );
 }
