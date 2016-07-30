9e0a37ba75f599240c316f0f79d28ed2f49b70ff
bsandusky <brett.sandusky@gmail.com>
2016-07-30 16:10:33 -0400

    typo

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,7 +5,7 @@ const SearchBar = (props) => {
   return (
     <div className="search-bar col-md-4">
       <section id="search">
-        <input id="search-input" placeholder="Seach recipes" onChange={(event) => props.filterList(event)}/>
+        <input id="search-input" placeholder="Search recipes" onChange={(event) => props.filterList(event)}/>
       </section>
     </div>
   );
